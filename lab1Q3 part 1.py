a8414a55bd269ec11f76c6518e7ca6e99a26bdb8
Anntara <64802731+anntara@users.noreply.github.com>
2020-09-18 14:52:46 -0400

    Update lab1Q3 part 1.py

--- lab1Q3 part 1.py
+++ lab1Q3 part 1.py
@@ -4,11 +4,9 @@ Created on Wed Sep 16 10:46:48 2020
 
 @author: Anntara Khan, Max Sours
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time 
-
 '''
 psuedo code
 A and B will be two constant matrices, with  N colums and rows, the marices will be created using numpy.ones.
@@ -20,29 +18,34 @@ and save it onto a list that will be used to plot the N vs Time plot.
 The for loop also multiplies the metrices A and B to Get the matrix C for every value of N.
 using matpltlib the N over Time is plotted, the slope of the graph is the speed of the program
 '''
-N = 200 #size of the matrix
-N1 = [0] #list to plot the y-axis
-A = np.ones([N, N], float)*3 #the matrix A
-B = np.ones([N, N], float)*5 #the matrix B
+N_range = np.arange(0,200) #array to plot the y-axis
 
-start1 = time() #saves the start time
-T = [start1] #list to plot the x-axis
-C = np.zeros([N,N] ,float) #defining the Matrix C
-#for loop to run the calculation
-for i in range(N):
-    for j in range(N):
-        for k in range(N):
-            C[i,j] += A[i,k]*B[k,j] #multiplies the matrix for 0-200
-            T.append(time()) #adds to the list everytime the for loop runs
-            N1.append(i) #adds to the list everytime the for loop runs
-end1=time() #saves the end time
-diff1 = (end1-start1) # the time it took for the function to run
-slope = N/diff1 #calculates the slope aka the speed of the program
-print ("therefore the speed of matrix multiplication is ", slope, "iterations per second") #prints the value of the slope
-plt.plot(T, N1) #plots the graph
+T = [] #list to plot the x-axis
+for N in N_range:
+    A = np.ones([N, N], float)*3 #the matrix A
+    B = np.ones([N, N], float)*5 #the matrix B
+    start1 = time() #saves the start time
+   
+    C = np.zeros([N,N] ,float) #defining the Matrix C
+    #for loop to run the calculation
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                C[i,j] += A[i,k]*B[k,j] #multiplies the matrix for 0-200
+                #N1.append(i) #adds to the list everytime the for loop runs
+    end1=time() #saves the end time
+    diff1 = (end1-start1) # the time it took for the function to run
+    T.append(diff1)
+plt.figure(1)
+plt.plot(N_range,T) #plots the graph
 plt.title('N over Time (matrix multiply)')
-plt.xlabel('time')
-plt.ylabel('N')
+plt.xlabel('N')
+plt.ylabel('Time')
+plt.figure(2)
+plt.plot(N_range**3,T) #plots the graph
+plt.title('N^3 over Time (matrix multiply)')
+plt.xlabel('N^3')
+plt.ylabel('Time')
 plt.savefig('N over Time (matrix multiply).png')
 plt.show()
 
