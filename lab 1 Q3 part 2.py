935309a62120a60d9abed6cc5b91cb1c3a68e39e
Anntara <64802731+anntara@users.noreply.github.com>
2020-09-17 22:04:39 -0400

    Update lab 1 Q3 part 2.py
    final change

--- lab 1 Q3 part 2.py
+++ lab 1 Q3 part 2.py
@@ -15,39 +15,30 @@ psuedo code
 A and B will be two constant matrices, with  N colums and rows, the marices will be created using numpy.ones.
 A will be a NxN matrix made of 3
 B will be a NxN matrix made of 5
-
+N is selected to be 200, we're using a for loop to run the through every value of N from 0 to 200, 
+in every iteration the for loop will save the time it took to run each iteration, 
+and save it onto a list that will be used to plot the N vs Time plot. 
+For the matrix multiplication numpydot is used, which calculates the dot product of 2 matrices in this case A and B, for every value of N.
+using matplotlib the N over Time is plotted, the slope of the graph is the speed of the program
 '''
+N = 200 #size of the matrix
+N1 = [0] #list to plot the y-axis
+A = np.ones([N, N], float)*3 #the matrix A
+B = np.ones([N, N], float)*5 #the matrix B
 
-N = 200
-N1 = [1]
-A = np.ones([N, N], float)*3
-B = np.ones([N, N], float)*5
-
-# save start time
-start1 = time()
-# run your calculation
-T = [start1] 
-
-# save start time
-start = time()
-# run your calculation
+start1 = time() #saves the start time
+T = [start1] #list to plot the x-axis
+#for loop to run the calculation
 for i in range(N):
-      C = np.dot(A,B)
-      T.append(time())
-      N1.append(i)
-      #print(C)
-
-
-# here are lines indented in the for loop
-# here are more lines indented in the for loop
-# save the end time
-end=time()
-# the difference is the elapsed time (in seconds)
-diff=end-start
-print (diff)
-
-plt.plot(T, N1)
+      C = np.dot(A,B) #find the dotproduct of matrix A & B for 0-200
+      T.append(time()) #adds to the list everytime the for loop runs
+      N1.append(i) #adds to the list everytime the for loop runs
+end1 = time() #saves the end time
+diff1 = (end1-start1) # the time it took for the function to run
+slope = n/diff1 #calculates the slope aka the speed of the program
+print (slope) #prints the value of the slope
+plt.plot(T, N1) #plots the graph
 plt.xlabel('time')
 plt.ylabel('N')
-plt.savefig('N over time.png')
+plt.savefig('N over Time (npdot).png')
 plt.show()
