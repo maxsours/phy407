005764c1245955da31e778715c4f214452586b56
Anntara <64802731+anntara@users.noreply.github.com>
2020-09-24 19:12:43 -0400

    Update lab2 Q1.py

--- lab2 Q1.py
+++ lab2 Q1.py
@@ -9,13 +9,23 @@ Created on Tue Sep 22 17:38:01 2020
 #LAB2 QUESTION 1
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.misc as sp
-
-DX = -np.exp(-(.5)**3)
+"""
+the derivative of the function, f(x) = e^(-x^2) is -2xe^(-x^2)
+the first fuction is to define f(x) in python for using in the the rest of the funtions
+the second fuction would take the first function and derive it by taking the slope of 
+a small change forward (x+h) in x.
+the 3rd fuction would take the first function and derive it by taking the slope of the 
+small change forward and backward 
+the functions are implemented for small change of h from 0 to 10^-16
+and then the error is taken by taking the difference between the analytic value and the
+numeric value (for both forward difference and central difference)
+"""
+x = 0.5
+DX = -2*x*(np.exp(-x**2))
 
 def func(x):
         return np.exp(-x**2)
-
+    
 def derive(x, h):
         return (func(x+h)-func(x))/h
     
@@ -25,15 +35,13 @@ def central_difference(x, h):
 h_range = np.logspace(-16, 0, num = 17)
 error = []
 
-
 for h in h_range:
-    
-    dx1 = derive(0.5, h)
+    dx1 = derive(x, h)
     error.append(dx1-DX)
+    e = np.abs(error)
 
 for h in h_range:
-    
-    dx1 = central_difference(0.5, h_range)
+    dx1 = central_difference(x, h_range)
     error1 = dx1-DX
     
 plt.loglog(h_range, error)
@@ -44,9 +52,8 @@ plt.ylabel('error')
 plt.savefig('steps over error.png')
 plt.show
 
-
-
-
+print (dx1)
+print (e)
 
     
 
