88bb1fdfa1cc2d80d5908f311e44d7956652c364
Anntara <64802731+anntara@users.noreply.github.com>
2020-09-24 23:01:17 -0400

    Update lab2 Q1.py

--- lab2 Q1.py
+++ lab2 Q1.py
@@ -20,30 +20,38 @@ the functions are implemented for small change of h from 0 to 10^-16
 and then the error is taken by taking the difference between the analytic value and the
 numeric value (for both forward difference and central difference)
 """
-x = 0.5
-DX = -2*x*(np.exp(-x**2))
+x = 0.5 #value of x is given
+DX = -2*x*(np.exp(-x**2)) #numeric derivation of the function
 
+#defining the function 
 def func(x):
         return np.exp(-x**2)
-    
+
+#defining the derivative function using forward difference scheme
 def derive(x, h):
         return (func(x+h)-func(x))/h
-    
+
+#defining the derivative function using central difference scheme
 def central_difference(x, h):
     return (func(x+h)-func(x-h))/h
 
+#creating an array for h
 h_range = np.logspace(-16, 0, num = 17)
+#setting an empty list for errors
 error = []
 
+#doing the derivation, and checking the error (Q1.C)
 for h in h_range:
     dx1 = derive(x, h)
     error.append(dx1-DX)
-    e = np.abs(error)
-
+    e = np.abs(error) #takes the absolute value of the error
+    
+#doing the derivation, and checking the error (Q1.D)
 for h in h_range:
     dx1 = central_difference(x, h_range)
     error1 = dx1-DX
-    
+
+#plotting everything togeather
 plt.loglog(h_range, error)
 plt.plot(error1, h_range)
 plt.title('comparing errors')
@@ -52,9 +60,9 @@ plt.ylabel('error')
 plt.savefig('steps over error.png')
 plt.show
 
+#prints out the value of the derivative and the error for Q1.B
 print (dx1)
 print (e)
-
     
 
 
