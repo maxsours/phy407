b1395761e519523cc1f1aa651a55775d6d661275
maxsours <maxsours@gmail.com>
2020-10-16 11:39:13 -0400

    Finished part b

--- Lab5/Lab05_Q1_a.py
+++ Lab5/Lab05_Q1_a.py
@@ -4,3 +4,29 @@ Created on Thu Oct 15 21:50:38 2020
 
 @author: Max Sours, Anntara Khan
 """
+import numpy as np
+import matplotlib.pyplot as plt
+
+sunspot_data = np.loadtxt("sunspots.txt")
+
+plt.plot(sunspot_data[:, 0], sunspot_data[:, 1])
+plt.xlabel("Months Since Jan 1749")
+plt.ylabel("Number of Sunspots")
+plt.title("Sunspot Observations")
+plt.show()
+
+sunspot_fft = np.fft.rfft(sunspot_data[: , 1])
+
+plt.semilogx(range(len(sunspot_fft)), np.abs(sunspot_fft) ** 2)
+plt.title("Fourier Transform of Sunspot Observations")
+plt.xlabel("Frequency")
+plt.ylabel("Amplitude")
+plt.show()
+
+sunspot_zoom = sunspot_fft[20:30]
+
+plt.plot(range(20, 30), np.abs(sunspot_zoom) ** 2)
+plt.title("Fourier Transform of Sunspot Observations (Zoomed In)")
+plt.xlabel("Frequency")
+plt.ylabel("Amplitude")
+plt.show()
---
+++ Lab5/Lab05_Q1_b.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 16 10:53:51 2020
+
+@author: Max Sours, Anntara Khan
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+dow = np.loadtxt("dow.txt")
+
+dow_fft = np.fft.rfft(dow)
+
+dow_fft[len(dow_fft) // 10:] = 0 # Set all but top 10% of terms to 0
+
+smoothed_dow10 = np.fft.irfft(dow_fft) # Get the inverse fourier transform of top 10% of terms
+
+dow_fft[len(dow_fft) // 50:] = 0  # Set all but top 2% of terms to 0
+
+smoothed_dow2 = np.fft.irfft(dow_fft) # Get the inverse transform with top 2% of terms
+
+#This is the plotting code
+plt.plot(range(len(dow)), dow, label = "Dow Data")
+plt.plot(range(len(smoothed_dow10)), smoothed_dow10, label = "Smoothed Dow (10%)")
+plt.plot(range(len(smoothed_dow2)), smoothed_dow2, label = "Smoothed Dow (2%)")
+plt.title("Dow from late 2006 to Dec 31 2010")
+plt.xlabel("Days since late 2006")
+plt.ylabel("Dow")
+plt.legend()
+plt.show()
