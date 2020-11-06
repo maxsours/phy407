7bc454e0d1ce57eb6ac764dcb41f9f8b72329338
maxsours <maxsours@gmail.com>
2020-11-05 23:35:01 -0500

    Added more comments

--- Lab8/lab08_Q1.py
+++ Lab8/lab08_Q1.py
@@ -18,21 +18,22 @@ def calculate_voltage(omega = 0):
     
     # Create arrays to hold potential values
     phi = np.zeros([M+1, M+1], float)
-    phi[20:80, 20] = V
+    phi[20:80, 20] = V # Initial conditions here
     phi[20:80, 80] = -1 * V
     
+    # Return true iff the square given by i, j is part of the boundary condition (not including if it's an edge)
     boundary_condition = lambda i, j: (j == 20 or j == 80) and i >= 20 and i < 80
     
     # Main loop
     delta = 1.0
     while delta > target:
-        delta = 0.0
-        # Calculate new values of the potential
+        delta = 0.0 # Needed to calculate maximum delta. This value should be immediately replaced in the first iteration
         for i in range(M+1):
             for j in range(M+1):
                 if not (i == 0 or i == M or j == 0 or j == M or boundary_condition(i, j)):
                     new_phi = (1 + omega) * (phi[i+1, j] + phi[i-1, j] 
                                              + phi[i, j+1] + phi[i, j-1])/4 - omega * phi[i, j]
+                    # Since there aren't two different arrays to compare, delta has to be calculated here and compared to the max value of delta found so far
                     new_delta = np.abs(new_phi - phi[i, j])
                     delta = new_delta if new_delta > delta else delta
                     phi[i, j] = new_phi
@@ -40,17 +41,20 @@ def calculate_voltage(omega = 0):
 
 if __name__ == "__main__":
     omega = 0.5
-    start = time()
+    start = time() # I'm timing how long the algorithm takes for each omega to compare the different values
     phi = calculate_voltage(omega)
     end = time()
     print("Time for omega = " + str(omega) + ":", end - start)
-    plt.contour(phi)
+    
+    plt.contour(phi) # Plot the contour plot
     plt.title("Voltage Contour Plot, $\omega$ = " + str(omega))
     plt.xlabel("x")
     plt.ylabel("y")
     cbar = plt.colorbar()
     cbar.set_label("Voltage (V)")
     plt.show()
+    
+    # Plot stream plot, as shown by the code in the Lab
     x = np.arange(101)
     Ey, Ex = np.gradient(-phi, x, x)
     # careful about order
